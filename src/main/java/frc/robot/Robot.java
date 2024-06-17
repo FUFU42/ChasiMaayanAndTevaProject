206f08f7ef8108f8b371fd4a2269c428600ef952
Maayan Donner <105926515+FUFU42@users.noreply.github.com>
2024-06-17 10:36:28 +0300

    Add files via upload

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,58 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.ChasiSubsystem;
+
+public class Robot extends TimedRobot {
+  
+  PS4Controller controller;
+  ChasiSubsystem chasiSubsystem;
+
+  @Override
+  public void robotInit() {
+    chasiSubsystem = new ChasiSubsystem();
+    controller = new PS4Controller(0);
+
+    chasiSubsystem.setDefaultCommand(new DriveCommand(chasiSubsystem, controller));
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
---
+++ src/main/java/frc/robot/subsystems/ChasiSubsystem.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ChasiSubsystem extends SubsystemBase {
+  
+  WPI_TalonFX FR;
+  WPI_TalonFX FL;
+  WPI_TalonFX RL;
+  WPI_TalonFX RR;
+  
+  public ChasiSubsystem() {
+    FR = new WPI_TalonFX(1);
+    FL = new WPI_TalonFX(2);
+    RL = new WPI_TalonFX(21);
+    RR = new WPI_TalonFX(11);
+  }
+  
+  public void drive(double speedL, double speedR){
+    FR.set(speedR);
+    FL.set(speedL);
+    RL.set(speedL);
+    RR.set(speedR);
+  }
+
+  public void stop(){
+    FR.stopMotor();
+    FL.stopMotor();
+    RL.stopMotor();
+    RR.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
